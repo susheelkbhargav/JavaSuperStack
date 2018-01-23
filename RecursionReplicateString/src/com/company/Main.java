15f4a7e33f81d8f157526986d45d8bd35b10eb32
susheelkbhargav <susheelkbhargav@gmail.com>
2018-01-22 17:25:39 -0800

    Code Modified for recursive function

--- RecursionReplicateString/src/com/company/Main.java
+++ RecursionReplicateString/src/com/company/Main.java
@@ -5,19 +5,16 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         String stringnum="a";
-        System.out.println("Final ="+Print(stringnum,8,stringnum));
+        System.out.println("Final ="+Print(stringnum,6));
     }
 
-    public static String Print(String string,int num, String originalString){
-        if(num==1){
-            System.out.println("base case called"+ string);
+    public static String Print(String string, int num) {
+        if (num <= 0)
+            return "";
+        if (num == 1)
             return string;
-        }
-        else{
-            string= string+string;
-            System.out.println(i+""+string);
-            i++;
-            return Print(string,num/2,originalString);
-        }
+
+        return Print(string + string, num / 2) +
+                Print(string, num % 2);
     }
 }
